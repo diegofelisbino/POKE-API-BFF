e2904ecaf57b81c2e0a337d23a09082141c3d507
Diego Felisbino <diego.felisbino@outlook.com>
2022-08-05 15:51:38 -0300

    alterado applicationUrl para o localhost

--- src/Pokedex.Api/Configurations/ApiConfig.cs
+++ src/Pokedex.Api/Configurations/ApiConfig.cs
@@ -5,7 +5,7 @@ namespace Pokedex.Api.Configurations
     public static class ApiConfig
     {
         public const string BASE_ADRESS_EXTERNAL_API = "https://pokeapi.co/api/v2";
-        public const string MEU_ENDERECO_BASE = "https://192.168.0.28:5001/";
+        public const string MEU_ENDERECO_BASE = "https://localhost:5001/";
         public const string MINHA_ROTA_BASE_V1 = "api/v1/pokemon/";
         
 
